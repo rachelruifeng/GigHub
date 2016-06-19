c72f95808ed29b346693901a733eaff346c4896f
Rachel Feng <rachel.feng@schneider-electric.com>
2016-06-19 13:16:44 +0930

    Add the gig to calendar.

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,40 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] AttendanceDto dto)
+        {
+            var attendeeId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == attendeeId && a.GigId == dto.GigId))
+            {
+                return BadRequest("The attendance already exists.");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = attendeeId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -1,14 +1,15 @@
 namespace GigHub.Models
 {
-    using System.Data.Entity;
-
     using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Data.Entity;
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Gig> Gigs { get; set; }
 
         public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -18,6 +19,16 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false); 
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
